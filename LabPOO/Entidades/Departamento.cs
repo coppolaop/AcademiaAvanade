6b28e1d71f8837f76afb26e869323107e4d152a7
Henrique Eduardo Souza <henrique.souza@gmail.com>
2021-06-29 21:24:51 -0300

    Configuração Projeto POO

---
+++ LabPOO/Entidades/Departamento.cs
@@ -0,0 +1,9 @@
+﻿namespace Entidades
+{
+    public class Departamento
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public string CentroCusto { get; set; }
+    }
+}
